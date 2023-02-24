b999063b5d49491396cf9ec941a6d07aae31af5f
VanehSann <vanessacbsantos22@gmail.com>
2023-02-24 01:55:13 -0300

    Ajustes na index alternativa

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,13 +2,16 @@ function showHideMenu() {
   const header = document.getElementsByTagName("header")[0]
   const navegacao = document.getElementById("navegacao")
   const menu = document.getElementById("menu")
+  const footer = document.getElementsByTagName("footer")[0]
   
   if (navegacao.style.display == "block") {
     navegacao.style.display = "none";
     header.classList.remove("open")
+    footer.classList.remove("open")
   } else {
     navegacao.style.display = "block";
     header.classList.add("open")
+    footer.classList.add("open")
 
   }
 
